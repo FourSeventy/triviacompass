54c985d179839b9d079d88138924d813a26e4b6f
Mike Signorella <michael@silvergobletgames.com>
2015-09-22 20:50:08 -0400

    rails hello world working

---
+++ vagrantScripts/app_specific.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Original script on https://github.com/orendon/vagrant-rails
+
+# load rbenv and shims
+export RBENV_ROOT="${HOME}/.rbenv"
+export PATH="${RBENV_ROOT}/bin:${PATH}"
+export PATH="${RBENV_ROOT}/shims:${PATH}"
+
+cd /vagrant
+bundle install
+rbenv rehash
+
+#set up database
+sudo sudo -u postgres psql -1 -c "CREATE USER trivia_user WITH PASSWORD 'password';"
+sudo sudo -u postgres psql -1 -c "ALTER USER trivia_user WITH SUPERUSER;"
+
+#fix database locale
+sudo sudo -u postgres psql -1 -c "update pg_database set datistemplate=false where datname='template1';"
+sudo sudo -u postgres psql -1 -c "drop database Template1;"
+sudo sudo -u postgres psql -1 -c "create database template1 with owner=postgres encoding='UTF-8' lc_collate='en_US.utf8' lc_ctype='en_US.utf8' template template0;"
+sudo sudo -u postgres psql -1 -c "update pg_database set datistemplate=true where datname='template1';"
+
+#create db
+rake db:create
+rake db:migrate
+rake db:seed
