1dc329129b2527815dda91b016425b4cb668614e
Mike Signorella <michael@silvergobletgames.com>
2015-10-06 22:04:45 -0400

    refactored a bunch of javascript

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,24 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+(function(homePage) {
+
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log('Latitude : ' + crd.latitude);
+        console.log('Longitude: ' + crd.longitude);
+        console.log('More or less ' + crd.accuracy + ' meters.');
+    };
+
+    function error(err) {
+        console.warn('ERROR(' + err.code + '): ' + err.message);
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+
+})(window.homePage = window.homePage || {});
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,53 +1,62 @@
-function initMap() {
-    var bostonLatLng = {lat: 42.360082, lng: -71.0589};
+(function(mapPage){
 
-    // Create a map object and specify the DOM element for display.
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: bostonLatLng,
-        //scrollwheel: false,
-        zoom: 13,
-        mapTypeControl: false,
-        streetViewControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
 
-    //get list of bars
     barList = [];
     barList = $('#bar-data').data('bars');
     infoWindowList = [];
+    markerList= [];
 
+    //init function that is called by the map api script after it is loaded
+    mapPage.initMap = function() {
 
-    //create marker for each bar
-    $.each(barList, function(index, bar){
+        var bostonLatLng = {lat: 42.360082, lng: -71.0589};
 
-        //create marker
-        var marker = new google.maps.Marker({
-            position: {lat: parseFloat(bar.lat), lng: parseFloat(bar.long)},
-            map: map,
-            animation: google.maps.Animation.DROP,
-            title: bar.name
+        // Create a map object and specify the DOM element for display.
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: bostonLatLng,
+            //scrollwheel: false,
+            zoom: 13,
+            mapTypeControl: false,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
-        //create info window
-        var infowindow = new google.maps.InfoWindow({
-            content: '<div class="marker-window"> <p>'+bar.name+'</p> <p>'+bar.address+'</p> <p>'+bar.city + ", " + bar.state + " " + bar.zip+'</p></div>'
-        });
+        //create marker for each bar
+        $.each(barList, function(index, bar){
 
-        infoWindowList.push(infowindow);
-
-        //bind click listener to open window
-        marker.addListener('click', function() {
-
-            //close other windows
-            $.each(infoWindowList, function(index, window){
-                window.close();
+            //create marker
+            var marker = new google.maps.Marker({
+                position: {lat: parseFloat(bar.lat), lng: parseFloat(bar.long)},
+                map: map,
+                animation: google.maps.Animation.DROP,
+                title: bar.name
             });
 
-            //open this one
-            infowindow.open(map, marker);
+            markerList.push(marker);
+
+
+            //create info window
+            var infowindow = new google.maps.InfoWindow({
+                content: '<div class="marker-window"> <p>'+bar.name+'</p> <p>'+bar.address+'</p> <p>'+bar.city + ", " + bar.state + " " + bar.zip+'</p></div>'
+            });
+
+            infoWindowList.push(infowindow);
+
+            
+            //bind click listener to open window
+            marker.addListener('click', function() {
+
+                //close other windows
+                $.each(infoWindowList, function(index, window){
+                    window.close();
+                });
+
+                //open this one
+                infowindow.open(map, marker);
+            });
+
         });
+    }
 
-    });
 
-
-}
+})(window.mapPage = window.mapPage || {});
