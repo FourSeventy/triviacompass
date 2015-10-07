7ad1b1cbcbd1f9d711789623a48599b35995d73f
Mike Signorella <michael@silvergobletgames.com>
2015-10-06 20:49:32 -0400

    added tonights trivia to home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
 
   def index
 
-    
+    today = Date.today.strftime('%A')
+    @tonights_bars = Bar.where(trivia_day: today).order(:trivia_time)
+
   end
 end
