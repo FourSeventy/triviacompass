64a5383a3c45650873dce649971fe0fdd414a485
Mike Signorella <michael@silvergobletgames.com>
2015-10-14 19:01:39 -0400

    adding functionality to ratings list action

--- app/controllers/rating_controller.rb
+++ app/controllers/rating_controller.rb
@@ -3,9 +3,36 @@ class RatingController < ApplicationController
   #GET ratings
   def list
 
+    #pull all ratings from the db
     all_ratings = Rating.all
 
+    #break ratings into hash of bar_id => [sum,number]
+    bar_hash = {}
+
+    all_ratings.each do |rating|
+
+      if bar_hash.has_key? rating.bar_id
+        arr = bar_hash[rating.bar_id]
+        arr[0] = arr[0] + rating.rating
+        arr[1] += 1
+      else
+        bar_hash[rating.bar_id] = [rating.rating, 1]
+      end
+
+    end
+
+    #iterate bar_hash sum the ratings and add them to final_rating_hash
+    final_rating_hash = {}
+    bar_hash.each do |k,v|
+
+      final_rating_hash[k] = (v[0].to_f / v[1])
+    end
+
+    #return the hash as json
+    render :json => final_rating_hash
+
   end
+
 
   #GET ratings/:id
   def get
@@ -24,11 +51,12 @@ class RatingController < ApplicationController
     render :json => total
 
   end
+  
 
   #POST ratings
   def createRating
 
-    Rating.create(bar_id: 1, rating: 4, ip: '192.168.0.1')
+    Rating.create(bar_id: 2, rating: 1, ip: '192.168.0.1')
 
     render :nothing => true
   end
