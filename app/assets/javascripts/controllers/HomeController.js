bc22e34a3abfd6ba50b93c8e2e3e134e4bdd938a
Mike Signorella <michael@silvergobletgames.com>
2015-11-11 23:05:19 -0500

    clear and populate map

--- app/assets/javascripts/controllers/HomeController.js
+++ app/assets/javascripts/controllers/HomeController.js
@@ -1,5 +1,8 @@
 app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
 
+    //map reference
+    $scope.map = null;
+
     //bar by day list
     $scope.barList = {};
     $scope.barList = $('#bar-data').data('bars');
@@ -13,6 +16,8 @@ app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
 
     $scope.clearList = function() {
         $scope.barList = {};
+
+        $scope.refreshMap();
     }
 
     $scope.populateList = function(){
@@ -23,39 +28,28 @@ app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
             url: '/bars'
         }).then(function successCallback(response) {
 
-            console.log(response);
             $scope.barList = response.data;
 
+            $scope.refreshMap();
+
         }, function errorCallback(response) {
+
+            console.log('Error getting bar list from server');
 
         });
 
     }
 
+    $scope.refreshMap = function() {
 
-
-
-    //init function that is called by the map api script after it is loaded
-    window.initMap = function() {
-
-
-        //if there is no map div on this page, return
-        if(!$('#map').length)
-        {
-            return;
+        //clear markers
+        for (var i = 0; i < markerList.length; i++) {
+            markerList[i].setMap(null);
         }
+        markerList = [];
 
-        var bostonLatLng = {lat: 42.360082, lng: -71.0589};
-
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: bostonLatLng,
-            //scrollwheel: false,
-            zoom: 13,
-            mapTypeControl: false,
-            streetViewControl: false,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
+        //clear info list
+        infoWindowList = [];
 
         //create marker for each bar
         $.each($scope.barList, function(day, bars){
@@ -64,7 +58,7 @@ app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
                 //create marker
                 var marker = new google.maps.Marker({
                     position: {lat: parseFloat(bar.lat), lng: parseFloat(bar.long)},
-                    map: map,
+                    map: $scope.map,
                     animation: google.maps.Animation.DROP,
                     title: bar.name
                 });
@@ -95,6 +89,36 @@ app.controller('HomeController', ['$scope', '$http', function($scope, $http) {
 
             });
         });
+    }
+
+
+
+
+    //init function that is called by the map api script after it is loaded
+    window.initMap = function() {
+
+
+        //if there is no map div on this page, return
+        if(!$('#map').length)
+        {
+            return;
+        }
+
+        var bostonLatLng = {lat: 42.360082, lng: -71.0589};
+
+        // Create a map object and specify the DOM element for display.
+        $scope.map = new google.maps.Map(document.getElementById('map'), {
+            center: bostonLatLng,
+            //scrollwheel: false,
+            zoom: 13,
+            mapTypeControl: false,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+
+        //refresh map
+        $scope.refreshMap();
 
     }
 
