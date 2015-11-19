209b2745a6e0c18df17bc1e36238435a69b3db1b
Mike Signorella <michael@silvergobletgames.com>
2015-11-19 18:42:33 -0500

    read cookie for default location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,22 +2,24 @@ class HomeController < ApplicationController
 
   def index
 
-    #get location
-         #use cookied location
-         #if not, use Boston as default
-    default_location = {name: 'Boston, MA', lat: 42.3583827, lng: -71.0626648 }
+    #set default location
+    location = {name: 'Boston, MA', lat: 42.3583827, lng: -71.0626648 }
 
-    #if we have a location, set proper header state
+    #override it if cookied location exists
+    cookie_location = cookies[:location]
+    unless cookie_location.nil?
+      location = JSON.parse cookie_location
+    end
 
     #get bars by location
-    radius = 10 #miles
-    bars = Bar.get_bars_in_radius(default_location[:lat],	default_location[:lng], radius)
+    radius = 30 #miles
+    bars = Bar.get_bars_in_radius(location[:lat],	location[:lng], radius)
 
     #organize bars into hash by day
     bars_by_day = Bar.sort_bars_into_days(bars)
 
     #assign default data to page
-    @default_data = {location: default_location, bars: bars_by_day}
+    @default_data = {location: location, bars: bars_by_day}
 
   end
 end
