95b23e1f453dc2bb383f043b4d3c605deddcf2f1
Mike Signorella <michael@silvergobletgames.com>
2015-11-19 20:15:18 -0500

    added some bar model unit tests

--- test/models/bar_test.rb
+++ test/models/bar_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class BarTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "field validation" do
+
+    bar = Bar.new(name: "Doyles", address: "3484 Washington St", city: "Jamacia Plain", state: "MA", zip: "02130",
+                  phone: nil, trivia_time: "7:00pm", trivia_day: "Monday", lat: "1", long: "2")
+    assert bar.valid?, "Bar validator failure"
+
+    bar = Bar.new(name: "Doyles", address: "3484 Washington St", city: "Jamacia Plain", state: "MA", zip: "02130",
+                  phone: "5085421111", trivia_time: "7:00pm", trivia_day: "Monday", lat: "1", long: "2")
+    assert bar.valid?, "Bar validator failure"
+
+    bar = Bar.new(name: "Doyles", address: "3484 Washington St", city: "Jamacia Plain", state: "MA", zip: "02130-1234",
+                  phone: "5085421111", trivia_time: "7:00pm", trivia_day: "Monday", lat: "1", long: "2")
+    assert bar.valid?, "Bar validator failure"
+  end
+
 end
