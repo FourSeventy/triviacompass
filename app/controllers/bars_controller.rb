1b22846bc75b53b901b888b09e099e0b589261e9
Mike <mike@starkiller.local>
2016-07-24 19:23:31 -0400

    refactoring some stuff

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -2,12 +2,10 @@ class BarsController < ApplicationController
 
   #GET /bars
   def index
-
     #get lat and long query params
     lat = params[:lat]
     long = params[:long]
     radius = params[:radius]
-
 
     #get bars from DB
     bars = Bar.get_bars_in_radius(lat.to_f, long.to_f, radius.to_f)
@@ -16,14 +14,13 @@ class BarsController < ApplicationController
     bars_by_day = Bar.sort_bars_into_days(bars)
 
     #return the hash as json
-    render :json => bars_by_day
-
+    render json: bars_by_day
   end
 
   #GET /bars/:id
   def show
     bar = Bar.find(params[:id])
 
-    render :json => bar
+    render json: bar
   end
 end
